66a281b42b7ce5644aab653b2ded5b4848e34d02
sudha-priya-coder <sudhapriyaofficial@gmail.com>
2024-06-03 10:37:34 +0530

    in changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const userRouter = require("./routes/user");
 const taskRouterr = require("./routes/task");
 
 const app = express();
-const port = process.env.port;
+const port = process.env.port || 3000;
 
 app.use(express.json());
 app.use(userRouter);
